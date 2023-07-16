3b1d772a3a9e31695ebc00178d520de0ff5679dc
Eric Hicks <ehicks05@yahoo.com>
2023-07-16 12:54:48 -0400

    add create user trigger

--- prisma/migrations/20210724140600_init/migration.sql
+++ prisma/migrations/20210724140600_init/migration.sql
@@ -133,3 +133,21 @@ CREATE OR REPLACE PROCEDURE updateScore() AS $$
 		update "Comment" set score=coalesce(getCommentScore("id", "createdAt"), 0);
 	END;
 $$ LANGUAGE plpgsql;
+
+-- inserts a row into recipebook.users
+create function recipebook.handle_new_user()
+returns trigger
+language plpgsql
+security definer set search_path = recipebook
+as $$
+begin
+  insert into recipebook.users (id)
+  values (new.id);
+  return new;
+end;
+$$;
+
+-- trigger the function every time a user is created
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure recipebook.handle_new_user();
