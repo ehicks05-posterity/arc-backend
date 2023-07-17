03e9b334cda8be36eee2865f299a3654a988c825
Eric Hicks <ehicks05@yahoo.com>
2023-07-17 17:18:08 -0400

    update apollo setup

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,24 +1,17 @@
 import { ApolloServer } from 'apollo-server-express';
 import { ApolloServerPluginInlineTrace } from 'apollo-server-core';
-import { createClient } from '@supabase/supabase-js';
 import { schema } from './schema';
 import prisma from './prisma';
-
-const supabase = createClient(
-  process.env.SUPABASE_URL || '',
-  process.env.SUPABASE_SERVICE_KEY || '',
-);
+import { supabase } from './supabase';
 
 const createApolloServer = () => {
   const server = new ApolloServer({
     schema,
     context: ({ req }: any) => {
-      console.log({ auth: req.auth });
       const user = req.auth
         ? {
             ...req.auth,
-            id: req.auth.app_metadata.username,
-            authId: req.auth.sub,
+            id: req.auth.sub,
           }
         : undefined;
 
