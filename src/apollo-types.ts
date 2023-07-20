631df7e3ff23558135cb0c4b8a2471ed9c47f180
Eric Hicks <ehicks05@yahoo.com>
2023-07-20 16:22:17 -0400

    clean up types

--- src/apollo-types.ts
+++ src/apollo-types.ts
@@ -1,14 +1,6 @@
 import { SupabaseClient, AuthUser } from '@supabase/supabase-js';
-import { PrismaClient, Prisma } from '@prisma/client';
-import { DefaultArgs } from '@prisma/client/runtime';
 
 export interface ApolloContext {
-  prisma: PrismaClient<
-    Prisma.PrismaClientOptions,
-    never,
-    Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined,
-    DefaultArgs
-  >;
   supabase: SupabaseClient<any, 'recipebook', any>;
   req: any;
   user?: AuthUser & { id: string };
