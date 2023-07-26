adaa22d37f2656cdff3f43ad69d3d4a1433590ca
Eric Hicks <ehicks05@yahoo.com>
2023-07-25 22:30:26 -0400

    set search path at top instead of inline

--- prisma/custom/custom.sql
+++ prisma/custom/custom.sql
@@ -1,62 +1,64 @@
+set search_path = arc;
+
 -- CreateFunction
-CREATE OR REPLACE FUNCTION arc.getAgeInHours(dt timestamptz) RETURNS integer AS $$
+CREATE OR REPLACE FUNCTION getAgeInHours(dt timestamptz) RETURNS integer AS $$
 	BEGIN
 		RETURN (EXTRACT(EPOCH FROM (now() - dt)) / 60 / 60);
 	END;
 $$ LANGUAGE plpgsql;
 
 -- CreateFunction
-CREATE OR REPLACE FUNCTION arc.getNetVotes(id text) RETURNS integer AS $$
+CREATE OR REPLACE FUNCTION getNetVotes(id text) RETURNS integer AS $$
 	DECLARE
 		netVotes integer = 0;
 	BEGIN
-		netVotes = (select sum(direction) from arc.user_post_vote where post_id = id);
+		netVotes = (select sum(direction) from user_post_vote where post_id = id);
 		RETURN coalesce(netVotes, 0);
 	END;
 $$ LANGUAGE plpgsql;
 
 -- CreateFunction
-CREATE OR REPLACE FUNCTION arc.getCommentNetVotes(id text) RETURNS integer AS $$
+CREATE OR REPLACE FUNCTION getCommentNetVotes(id text) RETURNS integer AS $$
 	DECLARE
 		netVotes integer = 0;
 	BEGIN
-		netVotes = (select sum(direction) from arc.user_comment_vote where comment_id = id);
+		netVotes = (select sum(direction) from user_comment_vote where comment_id = id);
 		RETURN coalesce(netVotes, 0);
 	END;
 $$ LANGUAGE plpgsql;
 
 -- CreateFunction
-CREATE OR REPLACE FUNCTION arc.getScore(id text, createdAt timestamptz) RETURNS real AS $$
+CREATE OR REPLACE FUNCTION getScore(id text, createdAt timestamptz) RETURNS real AS $$
 	BEGIN
-		RETURN arc.getNetVotes(id) / ((arc.getAgeInHours(createdAt) + 2) ^ 1.5);
+		RETURN getNetVotes(id) / ((getAgeInHours(createdAt) + 2) ^ 1.5);
 	END;
 $$ LANGUAGE plpgsql;
 
 -- CreateFunction
-CREATE OR REPLACE FUNCTION arc.getCommentScore(id text, createdAt timestamptz) RETURNS real AS $$
+CREATE OR REPLACE FUNCTION getCommentScore(id text, createdAt timestamptz) RETURNS real AS $$
 	BEGIN
-		RETURN arc.getCommentNetVotes(id) / ((arc.getAgeInHours(createdAt) + 2) ^ 1.5);
+		RETURN getCommentNetVotes(id) / ((getAgeInHours(createdAt) + 2) ^ 1.5);
 	END;
 $$ LANGUAGE plpgsql;
 
 -- CreateProcedure
-CREATE OR REPLACE PROCEDURE arc.updateScore() AS $$
+CREATE OR REPLACE PROCEDURE updateScore() AS $$
 	BEGIN
-		update arc.post set net_votes=coalesce(arc.getNetVotes("id", "createdAt"), 0);
-		update arc.post set score=coalesce(arc.getScore("id", "createdAt"), 0);
-		update arc.comment set net_votes=coalesce(arc.getCommentNetVotes("id", "createdAt"), 0);
-		update arc.comment set score=coalesce(arc.getCommentScore("id", "createdAt"), 0);
+		update post set net_votes=coalesce(getNetVotes("id", "createdAt"), 0);
+		update post set score=coalesce(getScore("id", "createdAt"), 0);
+		update comment set net_votes=coalesce(getCommentNetVotes("id", "createdAt"), 0);
+		update comment set score=coalesce(getCommentScore("id", "createdAt"), 0);
 	END;
 $$ LANGUAGE plpgsql;
 
--- inserts a row into arc.users
-create function arc.handle_new_user()
+-- inserts a row into users
+create function handle_new_user()
 returns trigger
 language plpgsql
 security definer set search_path = arc
 as $$
 begin
-  insert into arc.users (id)
+  insert into users (id)
   values (new.id);
   return new;
 end;
